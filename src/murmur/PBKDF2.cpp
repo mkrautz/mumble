bf99c2e610f6b0a24cb0b3f3c63639363e2eda87
Mikkel Krautz <mikkel@krautz.dk>
2016-05-08 17:23:54 +0200

    src/murmur: update files to use LICENSE.header.

--- src/murmur/PBKDF2.cpp
+++ src/murmur/PBKDF2.cpp
@@ -1,5 +1,9 @@
+// Copyright 2013-2016 The Mumble Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file at the root of the
+// Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
 /* Copyright (C) 2013, Morris Moraes <mauricioarozi@gmail.com>
-   Copyright (C) 2014, Stefan Hacker <dd0t@users.sourceforge.net>
 
    All rights reserved.
 
@@ -28,6 +32,7 @@
    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
+
 #include "murmur_pch.h"
 
 #include "PBKDF2.h"
