194fe20499abe1233f748821b3595baac32c5167
Mikkel Krautz <mikkel@krautz.dk>
2016-06-26 20:27:19 +0200

    Overlay: add launcher filter.
    The launcher filter is a step before the usual whitelist/blacklist.
    
    Users can maintain a list of allowed "launcher" processes, and Mumble
    will only inject the overlay into the process if that process's parent
    is part of this list of allowed launchers.
    
    The idea is that most games these days run through a launcher.
    
    Either the various game stores, or per-title launchers for some
    games.
    
    The hope is that this will remove a lot of the annoyances with
    the overlay feature -- at least for non-technical users. We've
    often had bug reports of Mumble injecting itself into things like
    browsers, producitiy apps, and so on. This change should ensure
    that, by default, the overlay only injects itself into games.

---
+++ overlay/overlay_launcherfilter.h
@@ -0,0 +1,20 @@
+// Copyright 2005-2016 The Mumble Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file at the root of the
+// Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
+#ifndef MUMBLE_OVERLAY_LAUNCHERFILTER_H_
+#define MUMBLE_OVERLAY_LAUNCHERFILTER_H_
+
+static const char *overlayLaunchers[] = {
+	"Steam.exe", // Steam
+	"EALaunchHelper.exe", // Origin
+	"Battle.net.exe", // Battle.net
+	"GalaxyClient.exe", // GOG Galaxy
+	"ffxivlauncher.exe", // Final Fantasy XIV Launcher
+	"UbisoftGameLauncher.exe", // Uplay
+	"UbisoftGameLauncher64.exe", // Uplay
+	NULL
+};
+
+#endif
