5a4e2f2a439eaa6e4afaf06bf9476d92acf50410
Mikkel Krautz <mikkel@krautz.dk>
2017-03-11 21:07:07 +0100

    overlay: teach the 'exclude' module to not consider the default whitelist when in WhitelistExclusionMode.

--- overlay/exclude.cpp
+++ overlay/exclude.cpp
@@ -183,8 +183,13 @@ std::vector<std::string> ExcludeGetLaunchers() {
 	return vexclude(vmerge(defaultLaunchers, userLaunchers), actualExcludedLaunchers);
 }
 
-std::vector<std::string> ExcludeGetWhitelist() {
+std::vector<std::string> ExcludeGetWhitelist(bool nodefault) {
 	std::vector<std::string> defaultWhitelist = vlowercase(defaultWhitelistVector());
+	// We don't consider Mumble's built-in whitelist when in WhitelistExclusionMode.
+	// The built-in whitelist is only used in LauncherFilterExclusionMode.
+	if (ExcludeGetMode() == WhitelistExclusionMode) {
+		defaultWhitelist = std::vector<std::string>();
+	}
 	std::vector<std::string> userWhitelist = vlowercase(regReadMultiString(HKEY_CURRENT_USER, "Software\\Mumble\\Mumble\\overlay", "whitelist"));
 	std::vector<std::string> userExcludedWhitelistEntries = vlowercase(regReadMultiString(HKEY_CURRENT_USER, "Software\\Mumble\\Mumble\\overlay", "whitelistexclude"));
 	std::vector<std::string> actualExcludedWhitelistEntries = vintersect(defaultWhitelist, userExcludedWhitelistEntries);
